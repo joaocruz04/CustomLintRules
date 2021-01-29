d2d0fe04607fa4c636c59793a6b87cae4fc975fe
joaocruz04 <joao.cruz@mercedes-benz.io>
2021-01-29 17:35:33 +0100

    Apply lint rules to sample App

---
+++ app/src/main/java/com/joaocruz04/customlintrules/MyDealership.kt
@@ -0,0 +1,20 @@
+package com.joaocruz04.customlintrules
+
+class MyDealership() {
+
+    // Bad class names below
+
+    class MyDealer
+    class My_Dealer_Stuff
+    class Dealer
+    class DealerList
+    class Dealers
+    class DealersList
+    class DealerShip
+
+    // Good class names below
+
+    class Dealership
+    class MyDealership
+    class My_Dealership_Stuff
+}
