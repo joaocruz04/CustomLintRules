a74f08660f5356eee3beb1c40d6909b03341e72a
joaocruz04 <joao.cruz@mercedes-benz.io>
2021-02-03 17:25:10 +0100

    Add lint exceptions

--- app/src/main/java/com/joaocruz04/customlintrules/MyCertificates.kt
+++ app/src/main/java/com/joaocruz04/customlintrules/MyCertificates.kt
@@ -6,4 +6,9 @@ class MyCertificates {
 
     val certificate1 = Certificate("mysha", "12/12/2020")
 
+    val certificate2 = Certificate("mysha", "12/12/2021")
+
+    val certificate3 = Certificate("mysha", "lala")
+
+    val certificate4 = Certificate("mysha", "12/04/2021")
 }
